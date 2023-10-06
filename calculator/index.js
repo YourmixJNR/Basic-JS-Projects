5771557bc1b5ed08119a9eac1dc088150de18937
Michael Victor <radmakson@gmail.com>
2023-10-06 13:12:46 +0100

    init

--- calculator/index.js
+++
@@ -1,9 +0,0 @@
-// let opy = document.querySelector('#operands');
-// let sobmit = document.querySelector('.submit')
-
-// sobmit.addEventListener('click', function(){
-//     // let opy = document.querySelector('#operands');
-//     console.log(opy.value);
-// })
-
-
